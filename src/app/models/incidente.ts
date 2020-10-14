341bb157c8148a05a2828bd1f41f18444005d6ea
denis123nahuamel <nahuamel.denis@gmail.com>
2020-10-14 12:22:47 -0500

    1 file modified

--- src/app/models/incidente.ts
+++ src/app/models/incidente.ts
@@ -1,7 +1,8 @@
 export interface IncidenteInterface {
   codIncidente: string;
-        fecha: string;
-        descripcion: string;
-        codDocente: string;
-        codEquipo: string;
+  fecha: string;
+  descripcion: string;
+  codDocente: string;
+  codEquipo: string;
+  solucionado: boolean;
 }
