4a2c1220dcb3c0c161d6846fa94748ddbb4a5870
denis123nahuamel <nahuamel.denis@gmail.com>
2020-10-15 08:59:30 -0500

    3 files modified

--- src/app/views/registrar-incidencia/registrar-incidencia.component.ts
+++ src/app/views/registrar-incidencia/registrar-incidencia.component.ts
@@ -1,77 +1,89 @@
-import { Component, OnInit } from '@angular/core';
-import { IncidenteInterface } from '../../models/incidente';
+import { Component, OnInit } from "@angular/core";
+import { IncidenteInterface } from "../../models/incidente";
 import { IncidenciaService } from "../../services/incidencia.service";
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormControl, Validators, NgForm } from "@angular/forms";
 @Component({
-  selector: 'app-registrar-incidencia',
-  templateUrl: './registrar-incidencia.component.html',
-  styleUrls: ['./registrar-incidencia.component.css']
+  selector: "app-registrar-incidencia",
+  templateUrl: "./registrar-incidencia.component.html",
+  styleUrls: ["./registrar-incidencia.component.css"]
 })
 export class RegistrarIncidenciaComponent implements OnInit {
-public nombreEquipo="";
-public titulo:string;
-public codIncidente:string;
-  constructor(private activatedRoute: ActivatedRoute
-    ,private incidenciaService: IncidenciaService, private router: Router) {
-      this.incidenciaService.codActualEquipo.subscribe(message => this.nombreEquipo = message)
-     }
+  public nombreEquipo = "";
+  public titulo: string;
+  public codIncidente: string;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private incidenciaService: IncidenciaService,
+    private router: Router
+  ) {
+    this.incidenciaService.codActualEquipo.subscribe(
+      message => (this.nombreEquipo = message)
+    );
+  }
 
   ngOnInit(): void {
-    this.codIncidente = this.activatedRoute.snapshot.paramMap.get('codIncidente');//idComponente
-    if(this.codIncidente){
-      this.titulo="Modificar Componente";
-      this.incidenciaService.getIncidencia(this.codIncidente).subscribe(incidente => {             
-         //this.nombreCatalogo=incidente.codCatalogo;
-         this.nombreEquipo=incidente.codEquipo;
-        
+    this.codIncidente = this.activatedRoute.snapshot.paramMap.get(
+      "codIncidente"
+    ); //idComponente
+    if (this.codIncidente) {
+      this.titulo = "Modificar Incidente";
+      this.incidenciaService
+        .getIncidencia(this.codIncidente)
+        .subscribe(incidente => {
+          //this.nombreCatalogo=incidente.codCatalogo;
+          this.nombreEquipo = incidente.codEquipo;
+
           this.postIncidencia.setValue({
-            
-             fecha: incidente.fecha,
-             descripcion:incidente.descripcion,
-             codEquipo:incidente.codEquipo,
-             codDocente:incidente.codDocente
-          });  
-      });
-    }
-    else{
-      this.titulo="Agregar Componente";
+            fecha: incidente.fecha,
+            descripcion: incidente.descripcion,
+            codEquipo: incidente.codEquipo,
+            codDocente: incidente.codDocente
+          });
+        });
+    } else {
+      this.titulo = "Agregar Incidente";
     }
   }
-   public postIncidencia = new FormGroup({
-    fecha: new FormControl('', Validators.required),
+  public postIncidencia = new FormGroup({
+    fecha: new FormControl("", Validators.required),
     descripcion: new FormControl(),
-    codDocente: new FormControl('', Validators.required),
-    codEquipo: new FormControl(''),
-    
+    codDocente: new FormControl("", Validators.required),
+    codEquipo: new FormControl("")
   });
- agregarIncidencia(incidente:IncidenteInterface){
-  	 if(this.codIncidente){//actualizar catalogo
-       this.incidenciaService.actualizarIncidencia(this.codIncidente,incidente).subscribe(res => {
-           console.log('Post updated successfully!');
-      });
-             this.irAtras();
+  agregarIncidencia(incidente: IncidenteInterface) {
+    if (this.codIncidente) {
+      //actualizar catalogo
+      this.incidenciaService
+        .actualizarIncidencia(this.codIncidente, incidente)
+        .subscribe(res => {
+          console.log("Post updated successfully!");
+        });
+      this.irAtras();
+    } else {
+      //agregar catalogo
+      incidente.codEquipo = this.nombreEquipo;
+      incidente.fecha = this.convertDateFormat(incidente.fecha);
+      console.log("incidencia", incidente);
+      this.incidenciaService.agregarIncidencia(incidente).subscribe(
+        result => {
+          this.router.navigate(["/docente/incidente"]);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+      this.irAtras();
+    }
   }
-  else{//agregar catalogo
-        incidente.codEquipo=this.nombreEquipo;
-      incidente.fecha=this.convertDateFormat(incidente.fecha);
-      console.log("incidencia",incidente);
-  	 this.incidenciaService.agregarIncidencia(incidente).subscribe((result) => {
-      this.router.navigate(['/docente/incidente']);
-    }, (err) => {
-      console.log(err);
-    });
-     this.irAtras();
- }
- }
- convertDateFormat(string) {
-  var info = string.split('-');
-  return info[0] + '-' + info[1] + '-' + info[2];
-}
-getEquipo(){
-       this.router.navigate(['equipo/seleccionar-equipo/2']); //opcion de incidente 
+  convertDateFormat(string) {
+    var info = string.split("-");
+    return info[0] + "-" + info[1] + "-" + info[2];
   }
-   irAtras(){
-  	this.router.navigate(['/docente/incidente']);
+  getEquipo() {
+    this.router.navigate(["equipo/seleccionar-equipo/2"]); //opcion de incidente
+  }
+  irAtras() {
+    this.router.navigate(["/docente/incidente"]);
   }
 }
